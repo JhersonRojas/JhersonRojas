be0b21b34fcf513fd25a4394f79307073b6374be
JhersonRojas <jhersonrojas.dev@gmail.com>
2024-01-15 21:59:35 -0500

    agregando patron y diseño para bienvenida inicial

--- me-astro/tailwind.config.mjs
+++ me-astro/tailwind.config.mjs
@@ -3,7 +3,16 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			backgroundSize: {
+				"patron-size-sm": "calc(99.9vw / 6) calc(99.9vw / 6);",
+				"patron-size-md": "calc(69.9vw / 9) calc(69.9vw / 9);"
+			},
+			backgroundImage: {
+				"patron-desing": "linear-gradient(rgb(161, 161, 170) 1px, transparent 1px), linear-gradient(to right, rgb(161, 161, 170) 1px, rgb(255, 255, 255) 1px);",
+				"patron-desing-dark": "linear-gradient(rgb(39, 39, 42) 1px, transparent 1px), linear-gradient(to right, rgb(39, 39, 42) 1px, rgb(0, 0, 0) 1px);"
+			}
+		},
 	},
 	plugins: [],
 }
