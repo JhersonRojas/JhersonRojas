1c52fd0749a9af70399e39fce3ce8931e3a862f8
JhersonRojas <jhersonrojas.dev@gmail.com>
2024-02-19 16:29:35 -0500

    añadiendo formateador de codigo

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
-	theme: {
-		extend: {
-			borderWidth: {
-				"min": "1px"
-			},
-			backgroundImage: {
-				"pixel": "url(/fondo.gif);",
-				"patron-design": "linear-gradient(rgb(161, 161, 170) 1px, transparent 1px), linear-gradient(to right, rgb(161, 161, 170) 1px, rgb(255, 255, 255) 1px);",
-				"patron-design-dark": "linear-gradient(rgb(39, 39, 42) 1px, transparent 1px), linear-gradient(to right, rgb(39, 39, 42) 1px, rgb(0, 0, 0) 1px);",
-			}
-		},
-	},
-	plugins: []
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    darkMode: "class",
+    theme: {
+        extend: {
+            borderWidth: {
+                min: "1px",
+            },
+            backgroundImage: {
+                pixel: "url(/fondo.gif);",
+                "patron-design":
+                    "linear-gradient(rgb(161, 161, 170) 1px, transparent 1px), linear-gradient(to right, rgb(161, 161, 170) 1px, rgb(255, 255, 255) 1px);",
+                "patron-design-dark":
+                    "linear-gradient(rgb(39, 39, 42) 1px, transparent 1px), linear-gradient(to right, rgb(39, 39, 42) 1px, rgb(0, 0, 0) 1px);",
+            },
+        },
+    },
+    plugins: [],
 }
